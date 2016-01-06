54d0e2a14c0a8659693a19e645aa6403acbd7847
Kevin Kragenbrink <kevin@gaikai.com>
2016-01-06 10:33:11 -0800

    Adding flip parser.

---
+++ src/parsers/dice/flip.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const Mersenne = require('mersenne');
+const MUSH = require('src/interfaces/TableMUSH');
+
+const util = require('util');
+
+class Flip {
+    constructor () {
+        this.result = null;
+    }
+
+    *parse () {
+        this.result = Mersenne.rand(2) ? 'Heads' : 'Tails';
+        return this.result;
+    }
+
+    toMUSH (dbref) {
+        MUSH.getInstance().oemit(dbref, util.format('\\[[ansi(<#00A0F0>, webroll)]\\] [name(%s)] flips a coin: %s', dbref, this.result));
+        MUSH.getInstance().pemit(dbref, util.format('\\[[ansi(<#00A0F0>, webroll)]\\] you flip a coin: %s', dbref, this.result));
+    }
+}
+
+module.exports = Flip;
